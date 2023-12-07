7b7a0787b483dd85b5bc74c5d36664f3db39aefa
Jon Ginty <j.ginty@outlook.com>
2023-12-07 12:00:09 +0000

    parsing utils

--- src/00/index.ts
+++ src/00/index.ts
@@ -1,5 +1,16 @@
-import { readInputLines } from "../utils/readlines.ts";
+import {
+  readInputCsv,
+  readInputLines,
+  readInputYaml,
+} from "../utils/readLines.ts";
 
 const input = await readInputLines();
 
 console.log(input[0]);
+
+const iy = await readInputYaml();
+console.log(iy);
+
+const csv = await readInputCsv();
+const numbers = csv.map((l) => l.map((n) => parseInt(n)));
+console.log(numbers);
--- src/utils/readLines.ts
+++ src/utils/readLines.ts
@@ -1,5 +1,6 @@
 import { indexRelative } from "./indexRelative.ts";
 import { splitInputLines } from "./splitInputLines.ts";
+import { parse } from "https://deno.land/std@0.208.0/yaml/mod.ts";
 
 export default async function readLines(filePath: string) {
   const text = await Deno.readTextFile(indexRelative(filePath));
@@ -9,4 +10,25 @@ export default async function readLines(filePath: string) {
 
 export async function readInputLines() {
   return await readLines("input.txt");
+}
+
+export async function readYaml(filePath: string) {
+  const text = await Deno.readTextFile(indexRelative(filePath));
+  return parse(text);
+}
+
+export async function readInputYaml() {
+  return await readYaml("input.yml");
+}
+
+export async function readDelimitedData(filePath: string, delimiter: string) {
+  return await (await readLines(filePath)).map((line) => line.split(delimiter));
+}
+
+export async function readDelimitedInputData(delimiter: string) {
+  return await readDelimitedData("input.txt", delimiter);
+}
+
+export async function readInputCsv() {
+  return await readDelimitedData("input.csv", ",");
 }
