f3001be8dfed9fb2f897a36388c5159de559f354
Jon Ginty <j.ginty@outlook.com>
2023-12-07 09:33:02 +0000

    day5 part 2

--- src/05p2/seedLogic.ts
+++ src/05p2/seedLogic.ts
@@ -1,30 +1,81 @@
 import { almanac, seedRange, thingMap } from "./parser.ts";
 
 export function findResult(seedList: seedRange[], al: almanac): number {
-  let total = 0;
-  seedList.forEach((r) => {
-    total += r[1];
+  // let total = 0;
+  // seedList.forEach((r) => {
+  //   total += r[1];
+  // });
+
+  // let globalCount = 0;
+
+  // //const results: number[] = [];
+  // let min = findResultOld(seedList[0][0], al);
+
+  // for (let i = 0; i < seedList.length; i++) {
+  //   const sr = seedList[i];
+  //   for (let j = sr[0]; j < sr[0] + sr[1]; j++) {
+  //     if (globalCount++ % 1000000 === 0) {
+  //       console.log(
+  //         `done ${globalCount} out of ${total}. Current lowest is ${min}`
+  //       );
+  //     }
+  //     min = Math.min(min, findResultOld(j, al));
+  //     //results.push(findResultOld(j, al))
+  //   }
+  // }
+  const results = findResultRange(seedList, al);
+  console.log(`
+  
+  
+  `);
+  console.log(results);
+  return Math.min(...results.map((r) => r[0]));
+}
+
+export function findResultRange(seeds: seedRange[], al: almanac): seedRange[] {
+  let ranges = seeds;
+  al.maps.forEach((map) => {
+    ranges = ranges
+      .map((range) => singleMapStepRange(range, map))
+      .reduce((p, c) => [...p, ...c]);
   });
+  return ranges;
+}
 
-  let globalCount = 0;
-
-  //const results: number[] = [];
-  let min = findResultOld(seedList[0][0], al);
-
-  for (let i = 0; i < seedList.length; i++) {
-    const sr = seedList[i];
-    for (let j = sr[0]; j < sr[0] + sr[1]; j++) {
-      if (globalCount++ % 1000000 === 0) {
-        console.log(
-          `done ${globalCount} out of ${total}. Current lowest is ${min}`
-        );
+export function singleMapStepRange(
+  seeds: seedRange,
+  map: thingMap
+): seedRange[] {
+  console.log(`input ${seeds}`);
+  const processedRanges: seedRange[] = [];
+  for (const line of map.lines) {
+    const seedStart = seeds[0];
+    const seedEnd = seedStart + seeds[1] - 1;
+    const mapEnd = line.source + line.range - 1;
+    const offset = line.dest - line.source;
+    if (seedStart >= line.source && seedStart <= mapEnd) {
+      if (seedEnd >= line.source && seedEnd <= mapEnd) {
+        // entire range is contained
+        processedRanges.push([seedStart + offset, seeds[1]]);
+        // console.log(`output ${processedRanges.join("---")}`);
+        // console.log("entire");
+        // console.log(map);
+        return processedRanges;
       }
-      min = Math.min(min, findResultOld(j, al));
-      //results.push(findResultOld(j, al))
+      seeds = [mapEnd + 1, seedEnd - mapEnd]; // new unprocessed range
+      processedRanges.push([seedStart + offset, mapEnd + 1 - seedStart]);
+    } else if (seedEnd >= line.source && seedEnd <= mapEnd) {
+      // trailing overlap
+      seeds = [seedStart, line.source - seedStart];
+      processedRanges.push([line.source + offset, seedEnd - line.source + 1]);
     }
   }
 
-  return min;
+  processedRanges.push(seeds);
+  // console.log(`output ${processedRanges.join("---")}`);
+  // console.log("partial");
+  // console.log(map);
+  return processedRanges;
 }
 
 export function findResultOld(seedNumber: number, al: almanac): number {
